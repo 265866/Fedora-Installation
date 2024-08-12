c96928d0f1ae1bfb7b6474213960ba860639acca
Colton <54053306+265866@users.noreply.github.com>
2024-08-12 01:45:38 -0700

    Create style.sh

---
+++ style.sh
@@ -0,0 +1,2 @@
+sudo dnf install gnome-tweak-tool svn rsync ostree libappstream-glib gtk3 gnome-themes-extra gtk-murrine-engine sassc
+wget -O /tmp/gnome_shell_integration.xpi "https://addons.mozilla.org/firefox/downloads/file/4300298/gnome_shell_integration-12.xpi" && firefox -install-addon /tmp/gnome_shell_integration.xpi && rm /tmp/gnome_shell_integration.xpi
