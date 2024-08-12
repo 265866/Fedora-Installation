5d037e28d505472064ee00b4eb54c2790c9c3578
Colton <54053306+265866@users.noreply.github.com>
2024-08-12 02:02:47 -0700

    Update style.sh

--- style.sh
+++ style.sh
@@ -1,2 +1,15 @@
 sudo dnf install gnome-tweak-tool svn rsync ostree libappstream-glib gtk3 gnome-themes-extra gtk-murrine-engine sassc
 git clone https://github.com/vinceliuice/Orchis-theme.git && cd Orchis-theme && ./install.sh && ./install.sh -l
+cd ..
+git clone https://github.com/265866/Fedora-Installation.git && cd Fedora-Installation
+unzip Orchis-shell-MOD.zip -d Orchis-shell-MOD
+unzip fonts.zip -d fonts
+unzip backgrounds.zip -d backgrounds
+unzip gnome-extension-settings.zip -d gnome-extension-settings
+cp -Rv Orchis-shell-MOD ~/themes
+cp -Rv fonts ~/.local/share/
+cp -Rv backgrounds ~/.local/share/
+dconf load /org/gnome/shell/extensions < gnome-extension-settings/gnome-extensions-settings.conf
+cd ..
+git clone https://github.com/TaylanTatli/Sevi.git && cd Sevi/ && ./install.sh -a
+cd ..
