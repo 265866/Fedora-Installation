5f34e73f6a4def22e2640c770ce6c9c9c4f64b36
Colton <54053306+265866@users.noreply.github.com>
2024-08-12 05:07:12 -0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -146,26 +146,164 @@ fi
 echo -e "${CYAN}Autoremoving unneeded dependencies${NC}"
 dnf autoremove -y
 
-mkdir -p ~/.local/share/backgrounds/.hidden && \
-wget -O ~/.local/share/backgrounds/.hidden/background.png https://raw.githubusercontent.com/265866/Fedora-Installation/main/background.png && \
-gsettings set org.gnome.desktop.background picture-uri "file:///home/$(whoami)/.local/share/backgrounds/.hidden/background.png" && \
-chmod 444 ~/.local/share/backgrounds/.hidden/background.png
-
-
 # Ask if user wants dark mode and theming and stuff
-if prompt_yes_no "${BLUE}Would you like to use darkmode? (y/n): ${NC}"; then
+if prompt_yes_no "${BLUE}Would you like to use a dark theme? (y/n): ${NC}"; then
     gsettings set org.gnome.desktop.interface color-scheme 'prefer-dark'
     mkdir -p ~/.local/share/backgrounds/.hidden
     wget -O ~/.local/share/backgrounds/.hidden/background.png https://raw.githubusercontent.com/265866/Fedora-Installation/main/background.png
     gsettings set org.gnome.desktop.background picture-uri "file:///home/$(whoami)/.local/share/backgrounds/.hidden/background.png"
     gsettings set org.gnome.desktop.background picture-uri-dark "file:///home/$(whoami)/.local/share/backgrounds/.hidden/background.png"
     chmod 444 ~/.local/share/backgrounds/.hidden/background.png
-    dnf install -y gnome-tweaks mpv socat
+    dnf install -y gnome-tweaks gtk3 gnome-themes-extra gtk-murrine-engine sassc
     flatpak install flathub com.mattjakeman.ExtensionManager -y
     
-    echo -e "${GREEN}Libreoffice suite removed${NC}"
+    echo -e "${GREEN}Dark theme added & Enabled${NC}"
 else
-    echo -e "${YELLOW}Skipping Libreoffice suite removal${NC}"
+    echo -e "${YELLOW}Skipping addding dark theme${NC}"
+fi
+
+# Ask if user wants telegran
+if prompt_yes_no "${BLUE}Would you like to install Telegram? (y/n): ${NC}"; then
+    dnf install -y telegram-desktop
+    echo -e "${GREEN}Telegram installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Telegram${NC}"
+fi
+
+# Ask if user wants golang
+if prompt_yes_no "${BLUE}Would you like to install Golang? (y/n): ${NC}"; then
+    dnf install -y golang
+    echo -e "${GREEN}Golang installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Golang${NC}"
+fi
+
+# Ask if user wants Visual Studio Code
+if prompt_yes_no "${BLUE}Would you like to install Visual Studio Code? (y/n): ${NC}"; then
+    sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+    echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | sudo tee /etc/yum.repos.d/vscode.repo > /dev/null
+    dnf check-update
+    dnf install code
+    echo -e "${GREEN}Visual Studio Code installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Visual Studio Code${NC}"
+fi
+
+# Ask if user wants Lunar Client
+if prompt_yes_no "${BLUE}Would you like to install Lunar Client? (y/n): ${NC}"; then
+    flatpak install flathub com.lunarclient.LunarClient -y
+    echo -e "${GREEN}Lunar Client installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Lunar Client${NC}"
+fi
+
+# Ask if user wants Prop Nvidia Drivers
+if prompt_yes_no "${BLUE}Would you like to install proprietary Nvidia Drivers? (y/n): ${NC}"; then
+    dnf install -y akmod-nvidia xorg-x11-drv-nvidia-cuda
+    echo -e "${GREEN}Nvidia Drivers installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Nvidia Drivers${NC}"
+fi
+
+# Ask if user wants Spotify
+if prompt_yes_no "${BLUE}Would you like to install Spotify? (y/n): ${NC}"; then
+    flatpak install flathub com.spotify.Client -y
+    echo -e "${GREEN}Spotify installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Spotify${NC}"
+fi
+
+# Ask if user wants Docker
+if prompt_yes_no "${BLUE}Would you like to install Docker? (y/n): ${NC}"; then
+    dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+    dnf install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+    systemctl start docker
+    echo -e "${GREEN}Docker installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Docker${NC}"
+fi
+
+# Ask if user wants Bun
+if prompt_yes_no "${BLUE}Would you like to install Bun? (y/n): ${NC}"; then
+    curl -fsSL https://bun.sh/install | bash
+    source ~/.bashrc
+    echo -e "${GREEN}Bun installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Bun${NC}"
+fi
+
+# Ask if user wants NodeJS
+if prompt_yes_no "${BLUE}Would you like to install NodeJS? (y/n): ${NC}"; then
+    dnf install -y nodejs
+    echo -e "${GREEN}Bun installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Bun${NC}"
+fi
+
+# Ask if user wants Rust
+if prompt_yes_no "${BLUE}Would you like to install Rust? (y/n): ${NC}"; then
+    curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+    . "$HOME/.cargo/env"
+    echo -e "${GREEN}Rust installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Rust${NC}"
+fi
+
+# Ask if user wants OBS
+if prompt_yes_no "${BLUE}Would you like to install OBS? (y/n): ${NC}"; then
+    dnf install -y obs-studio
+    echo -e "${GREEN}OBS installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing OBS${NC}"
+fi
+
+
+# Ask if user wants Wireguard
+if prompt_yes_no "${BLUE}Would you like to install Wireguard? (y/n): ${NC}"; then
+    dnf install -y wireguard-tools
+    echo -e "${GREEN}Wireguard installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Wireguard${NC}"
+fi
+
+# Ask if user wants Steam
+if prompt_yes_no "${BLUE}Would you like to install Steam? (y/n): ${NC}"; then
+    dnf install -y steam
+    echo -e "${GREEN}Steam installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Steam${NC}"
+fi
+
+# Ask if user wants Python3
+if prompt_yes_no "${BLUE}Would you like to install Python3? (y/n): ${NC}"; then
+    dnf install -y python3
+    echo -e "${GREEN}Python3 installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Python3${NC}"
+fi
+
+# Ask if user wants Discord
+if prompt_yes_no "${BLUE}Would you like to install Discord? (y/n): ${NC}"; then
+    flatpak install flathub com.discordapp.Discord -y
+    echo -e "${GREEN}Discord installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Discord${NC}"
+fi
+
+# Ask if user wants Prism Launcher
+if prompt_yes_no "${BLUE}Would you like to install Prism Launcher (Basically MultiMC)? (y/n): ${NC}"; then
+    flatpak install flathub org.prismlauncher.PrismLauncher -y
+    echo -e "${GREEN}Prism Launcher installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing Prism Launcher${NC}"
+fi
+
+# Ask if user wants VLC
+if prompt_yes_no "${BLUE}Would you like to install VLC? (y/n): ${NC}"; then
+    flatpak install flathub org.videolan.VLC -y
+    echo -e "${GREEN}VLC installed${NC}"
+else
+    echo -e "${YELLOW}Skipping installing VLC${NC}"
 fi
 
 
