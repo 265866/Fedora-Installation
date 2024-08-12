4d09eca46647ef69e3e3dc94371a9ad5989df0aa
Colton <54053306+265866@users.noreply.github.com>
2024-08-12 01:50:21 -0700

    Update style.sh

--- style.sh
+++ style.sh
@@ -1,1 +1,2 @@
 sudo dnf install gnome-tweak-tool svn rsync ostree libappstream-glib gtk3 gnome-themes-extra gtk-murrine-engine sassc
+git clone https://github.com/vinceliuice/Orchis-theme.git && cd Orchis-theme && ./install.sh && ./install.sh -l
